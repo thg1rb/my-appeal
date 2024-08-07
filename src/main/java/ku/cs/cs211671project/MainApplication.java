39b1ed6f53c19c8fd3e1773c95c67b1bc6ce1f89
aock Chaiyawat <aock2547@gmail.com>
2024-08-07 18:22:48 +0700

    add: admin Views and admin Basic Controllers

--- src/main/java/ku/cs/cs211671project/MainApplication.java
+++ src/main/java/ku/cs/cs211671project/MainApplication.java
@@ -11,14 +11,25 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXRouter.bind(this, stage, "CS211 Project", 1024, 768);
+        FXRouter.bind(this, stage, "CS211 Project", 1440, 832);
         configRoutes();
-        FXRouter.goTo("hello");
+        FXRouter.goTo("login");
     }
 
     private void configRoutes() {
         String viewPath = "ku/cs/views/";
         FXRouter.when("hello", viewPath + "hello-view.fxml");
+
+        // General
+        FXRouter.when("login", viewPath + "login.fxml");
+        FXRouter.when("register-personal-data", viewPath + "register-personal-data.fxml");
+        FXRouter.when("register-username-password", viewPath + "register-username-password.fxml");
+
+        // Admin
+        FXRouter.when("admin-user-manage", viewPath + "admin-user-management.fxml");
+        FXRouter.when("admin-faculty-manage", viewPath + "admin-faculty-management.fxml");
+        FXRouter.when("admin-staff-manage", viewPath + "admin-staff-management.fxml");
+        FXRouter.when("admin-dashboard", viewPath + "admin-dashboard.fxml");
     }
 
     public static void main(String[] args) {
