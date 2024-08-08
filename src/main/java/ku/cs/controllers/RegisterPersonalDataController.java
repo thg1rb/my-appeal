f4839515045f43e717be85f2d48283114f099ad4
Bowornrat Tangnararatchakit <bowornrat.t@ku.th>
2024-08-08 16:13:27 +0700

    fix: register Views and Controllers and edit README

--- src/main/java/ku/cs/controllers/RegisterPersonalDataController.java
+++ src/main/java/ku/cs/controllers/RegisterPersonalDataController.java
@@ -1,4 +1,20 @@
 package ku.cs.controllers;
 
+import javafx.fxml.FXML;
+import ku.cs.services.FXRouter;
+
+import java.io.IOException;
+
 public class RegisterPersonalDataController {
+
+    // ไปที่หน้าลงทะเบียนถัดไป
+    @FXML
+    public void onNextButtonClick() {
+        try {
+            FXRouter.goTo("register-username-password");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
