80aec47cb9f81c3c2c08981375b0b56b6cebfe6d
Bowornrat <bowornrat.t@ku.th>
2024-08-26 17:41:18 +0700

    add: Appeal classes

---
+++ src/main/java/ku/cs/models/Appeal.java
@@ -0,0 +1,15 @@
+package ku.cs.models;
+
+public class Appeal {
+    protected String type;  // ประเภทคำร้อง
+
+    // Setter
+    protected void setType(String type) {
+        this.type = type;
+    }
+
+    // Getter
+    protected String getType() {
+        return this.type;
+    }
+}
