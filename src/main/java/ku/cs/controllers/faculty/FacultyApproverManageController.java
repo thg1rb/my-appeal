da5cc3cc778a780d4598342d6c3946f69faa53bd
karnesque <komchan.v@ku.th>
2024-08-27 22:13:21 +0700

    add: faculty approver popup

--- src/main/java/ku/cs/controllers/faculty/FacultyApproverManageController.java
+++ src/main/java/ku/cs/controllers/faculty/FacultyApproverManageController.java
@@ -1,11 +1,30 @@
 package ku.cs.controllers.faculty;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
 import ku.cs.services.FXRouter;
 
 import java.io.IOException;
 
 public class FacultyApproverManageController {
+
+
+    @FXML private Pane backgroundAddApproverPane;
+
+    @FXML private Pane addApproverPane;
+
+
+    @FXML
+    public void addApproverButton(){
+        backgroundAddApproverPane.setVisible(true);
+        addApproverPane.setVisible(true);
+    }
+
+    @FXML
+    public void onCloseButtonClick(){
+        backgroundAddApproverPane.setVisible(false);
+        addApproverPane.setVisible(false);
+    }
 
     @FXML
     public void onAppealButtonClick(){
