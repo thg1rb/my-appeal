2bc96b01b0778ae2de15d9442b7597e4c4aa2b92
aock Chaiyawat <aock2547@gmail.com>
2024-08-29 02:26:43 +0700

    add: TableView in adminStaffMngPage can show Staff

--- src/main/java/ku/cs/models/users/Professor.java
+++ src/main/java/ku/cs/models/users/Professor.java
@@ -1,5 +1,6 @@
 package ku.cs.models.users;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import ku.cs.services.FXRouter;
 
 import java.io.IOException;
@@ -8,9 +9,13 @@ public class Professor extends User {
     private String faculty;
     private String major;
     private String id;
+    private String initPassword;
+    private String initPasswordText;
 
     public Professor(String username, String password, String firstName, String lastName, String faculty, String major, String id) {
         super(username, password, firstName, lastName, "อาจารย์ที่ปรึกษา");
+        setInitPassword(password);
+        this.initPasswordText = password;
         this.faculty = faculty;
         this.major = major;
         this.id = id;
@@ -19,6 +24,11 @@ public class Professor extends User {
     public String getFaculty() { return faculty; }
     public String getMajor() { return major; }
     public String getId() { return id; }
+    public String getInitPasswordText() { return initPasswordText; }
+
+    public void setInitPassword(String password) {
+        this.initPassword = BCrypt.withDefaults().hashToString(12, password.toCharArray());
+    }
 
     @Override
     public void login() {
