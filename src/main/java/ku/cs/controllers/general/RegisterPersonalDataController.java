7ccc16a14b438efd4d97d50dcf10e0fbf7c2a255
aock Chaiyawat <aock2547@gmail.com>
2024-08-30 21:14:32 +0700

    registerPage get datasource

--- src/main/java/ku/cs/controllers/general/RegisterPersonalDataController.java
+++ src/main/java/ku/cs/controllers/general/RegisterPersonalDataController.java
@@ -1,12 +1,24 @@
 package ku.cs.controllers.general;
 
 import javafx.fxml.FXML;
+
+import ku.cs.models.collections.StudentList;
+
+import ku.cs.services.Datasource;
 import ku.cs.services.FXRouter;
+import ku.cs.services.StudentRosterListFileDatasource;
 
 import java.io.IOException;
 
 public class RegisterPersonalDataController {
+    private Datasource<StudentList> datasource;
+    private StudentList studentList;
 
+    @FXML
+    public void initialize() {
+        datasource = new StudentRosterListFileDatasource("data", "student");
+
+    }
     // ไปที่หน้าลงทะเบียนถัดไป
     @FXML
     public void onNextButtonClick() {
