bc69646cfecf1f630f4d6e8c82654d6964d4de4c
aock Chaiyawat <aock2547@gmail.com>
2024-08-31 15:26:25 +0700

    add: basic Register Feature

--- src/main/java/ku/cs/controllers/general/RegisterPersonalDataController.java
+++ src/main/java/ku/cs/controllers/general/RegisterPersonalDataController.java
@@ -2,30 +2,66 @@ package ku.cs.controllers.general;
 
 import javafx.fxml.FXML;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 import ku.cs.models.collections.StudentList;
+import ku.cs.models.collections.UserList;
+import ku.cs.models.persons.Student;
 
 import ku.cs.services.Datasource;
 import ku.cs.services.FXRouter;
 import ku.cs.services.StudentRosterListFileDatasource;
+import ku.cs.services.UserListFileDatasource;
 
 import java.io.IOException;
 
 public class RegisterPersonalDataController {
-    private Datasource<StudentList> datasource;
+    @FXML private TextField firstNameTextField;
+    @FXML private TextField lastNameTextField;
+    @FXML private TextField emailTextField;
+    @FXML private TextField idTextField;
+
+    @FXML private Label errorLabel;
+
+    private Datasource<UserList> userDatasource;
+    private UserList userList;
+
+    private Datasource<StudentList> studentListDatasource;
     private StudentList studentList;
 
     @FXML
     public void initialize() {
-        datasource = new StudentRosterListFileDatasource("data", "student");
+        userDatasource = new UserListFileDatasource("data", "user.csv");
+        userList = userDatasource.readData();
 
+        studentListDatasource = new StudentRosterListFileDatasource("data", "student-roster.csv");
+        studentList = studentListDatasource.readData();
+
+        errorLabel.setText("");
     }
+
     // ไปที่หน้าลงทะเบียนถัดไป
     @FXML
     public void onNextButtonClick() {
-        try {
-            FXRouter.goTo("register-username-password");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        String firstName = firstNameTextField.getText();
+        String lastName = lastNameTextField.getText();
+        String email = emailTextField.getText();
+        String id = idTextField.getText();
+
+        Student exist = studentList.findStudentByInformation(firstName, lastName, id, email);
+        if (exist != null) {
+            if (userList.findUserByPersonalInformation(firstName, lastName, id,email) == null){
+                try {
+                    FXRouter.goTo("register-username-password", exist);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }else{
+                errorLabel.setText("ท่านได้ทำการลงทะเบียนไปแล้ว");
+            }
+        }else {
+            errorLabel.setText("*ข้อมูลของท่านไม่อยู่ในระบบ กรุณาติดต่อเจ้าหน้าที่");
         }
     }
 
--- src/main/java/ku/cs/services/UserListHardCodeDatasource.java
+++ src/main/java/ku/cs/services/UserListHardCodeDatasource.java
@@ -18,11 +18,11 @@ public class UserListHardCodeDatasource implements Datasource<UserList> {
         list.addUser(new User("เจ้าหน้าที่ภาควิชา", "major3", "user3", "จนท.คณะ", "เองงับ", "วิศวกรรมศาสตร์", "วิศวกรรมไฟฟ้า"));
         list.addUser(new User("เจ้าหน้าที่ภาควิชา", "major4", "user4", "จนท.คณะ", "เองงับ", "วิศวกรรมศาสตร์", "วิศวกรรมเครื่องกล"));
         list.addUser(new User("เจ้าหน้าที่ภาควิชา", "major5", "user5", "จนท.คณะ", "เองงับ", "วิศวกรรมศาสตร์", "วิศวกรรมโยธา"));
-        list.addUser(new User("อาจารย์ที่ปรึกษา", "Bright", "professor", "ไบร้ง้าบ", "เป็นอาจารย์", "วิทยาศาสตร์", "วิทยาการคอมพิวเตอร์", "6610402132"));
+        list.addUser(new User("อาจารย์ที่ปรึกษา", "BrightPro", "professor", "ไบร้ง้าบ", "เป็นอาจารย์", "วิทยาศาสตร์", "วิทยาการคอมพิวเตอร์", "6610402132"));
         list.addUser(new User("อาจารย์ที่ปรึกษา", "professor1", "test1", "อาจารย์", "ชอบสอน", "วิทยาศาสตร์", "สถิติ", "12345678"));
         list.addUser(new User("อาจารย์ที่ปรึกษา", "professor2", "test2", "อาจารย์", "ชอบสอน", "วิศวกรรมศาสตร์", "วิศวกรรมไฟฟ้า", "12345678"));
         list.addUser(new User("อาจารย์ที่ปรึกษา", "professor3", "test3", "อาจารย์", "ชอบสอน", "เศรษฐศาสตร์", "เศรษฐศาสตร์", "12345678"));
-        list.addUser(new User("นักศึกษา", "Bright", "student", "ไบร้", "เป็นนิสิต", "วิทยาศาสตร์", "วิทยาการคอมพิวเตอร์", "6610401985", "bright@email.test"));
+        list.addUser(new User("นักศึกษา", "BrightStu", "student", "ไบร้", "เป็นนิสิต", "วิทยาศาสตร์", "วิทยาการคอมพิวเตอร์", "6610401985", "bright@email.test"));
         list.addUser(new User("นักศึกษา", "nisit1", "test1", "นิสิต", "สมมติ1", "วิทยาศาสตร์", "สถิติ", "6610401xxx", "nisit@email.test"));
         list.addUser(new User("นักศึกษา", "nisit2", "test2", "นิสิต", "สมมติ2", "วิทยาศาสตร์", "สถิติ", "6610401xxx", "nisit@email.test"));
         list.addUser(new User("นักศึกษา", "nisit3", "test3", "นิสิต", "สมมติ3", "วิทยาศาสตร์", "สถิติ", "6610401xxx", "nisit@email.test"));
