6f8f8d31cbb5660e56a7c30a129ab01db76a19c8
aock Chaiyawat <aock2547@gmail.com>
2024-09-06 10:09:58 +0700

    fix: add pom.xml files that make JAR file can run

--- src/main/java/ku/cs/services/DateTimeService.java
+++ src/main/java/ku/cs/services/DateTimeService.java
@@ -3,8 +3,6 @@ package ku.cs.services;
 import java.time.LocalDateTime;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.Date;
 
 public class DateTimeService {
@@ -25,28 +23,15 @@ public class DateTimeService {
         return detailedFormatter.format(date);
     }
 
-//    public static int compareDate(String date1, String date2){
-//        try{
-//            Date dateTime1 = detailedFormatter.parse(date1);
-//            Date dateTime2 = detailedFormatter.parse(date2);
-//            return dateTime1.compareTo(dateTime2);
-//        }
-//        catch(Exception e){
-//            e.printStackTrace();
-//            return date1.compareTo(date2);
-//        }
-//
-//    }
-public static int compareDate(String date1, String date2) {
-    try {
-        Date dateTime1 = detailedFormatter.parse(date1);
-        Date dateTime2 = detailedFormatter.parse(date2);
-        return dateTime2.compareTo(dateTime1);
-    } catch (ParseException e) {
-        e.printStackTrace();
-        return 0; // Or use a default string comparison as a fallback: date1.compareTo(date2)
+    public static int compareDate(String date1, String date2) {
+        try {
+            Date dateTime1 = detailedFormatter.parse(date1);
+            Date dateTime2 = detailedFormatter.parse(date2);
+            return dateTime2.compareTo(dateTime1);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
-}
-
 
 }
