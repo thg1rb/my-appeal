d2846a19308bdb58093a563029c6a304e3e91bd8
karnesque <komchan.v@ku.th>
2024-09-23 15:25:48 +0700

    added theme choice

--- src/main/java/ku/cs/controllers/general/ProgramSettingController.java
+++ src/main/java/ku/cs/controllers/general/ProgramSettingController.java
@@ -1,11 +1,12 @@
 package ku.cs.controllers.general;
 
-
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.Pane;
+import ku.cs.cs211671project.MainApplication;
 import ku.cs.models.persons.User;
 import ku.cs.services.FXRouter;
 
@@ -18,6 +19,7 @@ public class ProgramSettingController {
 
     @FXML private Pane navbarAnchorPane;
     private User user;
+    private Scene scene; // The main scene of the app
 
     @FXML
     public void initialize() {
@@ -29,20 +31,19 @@ public class ProgramSettingController {
         try {
             Pane navbarComponent = navbarComponentLoader.load();
             navbarAnchorPane.getChildren().add(navbarComponent);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
-        themeChoiceBox.getItems().addAll("Dark Theme", "Light Theme");
+        // ตั้งค่าตัวเลือกใน ChoiceBox
+        themeChoiceBox.getItems().addAll("Dark", "Light");
 
-        fontChoiceBox.getItems().addAll("Arial", "Times New Roman", "Verdana");
+        fontChoiceBox.getItems().addAll();
 
-        fontSizeChoiceBox.getItems().addAll("12px", "16px", "20px", "24px");
+        fontSizeChoiceBox.getItems().addAll("Small", "Medium", "Large");
 
 
     }
 
 
 }
-
-
--- src/main/java/ku/cs/cs211671project/MainApplication.java
+++ src/main/java/ku/cs/cs211671project/MainApplication.java
@@ -1,14 +1,14 @@
 package ku.cs.cs211671project;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import ku.cs.services.FXRouter;
 
 import java.io.IOException;
 
 public class MainApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXRouter.bind(this, stage, "CS211 Project", 1440, 832);
@@ -30,7 +30,7 @@ public class MainApplication extends Application {
         FXRouter.when("admin-user-manage", viewPath + "admin/" + "admin-user-management.fxml");
         FXRouter.when("admin-faculty-manage", viewPath + "admin/" + "admin-faculty-management.fxml");
         FXRouter.when("admin-staff-manage", viewPath + "admin/" + "admin-staff-management.fxml");
-        FXRouter.when("admin-dashboard", viewPath + "admin/"  + "admin-dashboard.fxml");
+        FXRouter.when("admin-dashboard", viewPath + "admin/" + "admin-dashboard.fxml");
 
         // Student
         FXRouter.when("student-track-appeal", viewPath + "student/" + "student-track-appeal.fxml");
@@ -48,6 +48,7 @@ public class MainApplication extends Application {
         FXRouter.when("major-appeal-manage", viewPath + "major/" + "major-appeal-manage.fxml");
         FXRouter.when("major-approver-manage", viewPath + "major/" + "major-approver-manage.fxml");
         FXRouter.when("major-nisit-manage", viewPath + "major/" + "major-nisit-manage.fxml");
+
     }
 
     public static void main(String[] args) {
