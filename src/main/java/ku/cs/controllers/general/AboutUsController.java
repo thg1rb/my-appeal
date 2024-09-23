66410a7152ce21e295f126919e7a3d6759ffeb6e
karnesque <komchan.v@ku.th>
2024-09-23 15:55:04 +0700

    add initialize to controller

--- src/main/java/ku/cs/controllers/general/AboutUsController.java
+++ src/main/java/ku/cs/controllers/general/AboutUsController.java
@@ -1,4 +1,33 @@
 package ku.cs.controllers.general;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+import ku.cs.models.persons.User;
+import ku.cs.services.FXRouter;
+
 public class AboutUsController {
+
+    private User user;
+    @FXML private Pane navbarAnchorPane;
+
+
+    @FXML
+    public void initialize() {
+        user = (User) FXRouter.getData();
+
+        //NavBar Component
+        String role = user.getRoleInEnglish();
+        FXMLLoader navbarComponentLoader = new FXMLLoader(getClass().getResource("/ku/cs/views/general/" + role + "-navbar.fxml"));
+        try {
+            Pane navbarComponent = navbarComponentLoader.load();
+            navbarAnchorPane.getChildren().add(navbarComponent);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+    }
 }
