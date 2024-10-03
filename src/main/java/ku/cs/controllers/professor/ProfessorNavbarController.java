7c61fba69fe49abf40b390544c82f18a6232424a
Bowornrat <bowornrat.t@ku.th>
2024-10-03 16:46:10 +0700

    fix: style some controllers and change static components to dynamic

--- src/main/java/ku/cs/controllers/professor/ProfessorNavbarController.java
+++ src/main/java/ku/cs/controllers/professor/ProfessorNavbarController.java
@@ -50,7 +50,7 @@ public class ProfessorNavbarController {
     }
 
     @FXML
-    void onProgramSettingClicked(){
+    void onProgramSettingButtonClicked(){
         try {
             FXRouter.goTo("program-setting",user);
         } catch (IOException e) {
