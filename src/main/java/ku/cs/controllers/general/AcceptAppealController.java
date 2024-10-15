bc91e75715857d4f1fd0f2b14ba02a6e6658fb91
Tonwantpillow <kerdsiri.s@ku.th>
2024-10-15 20:19:44 +0700

    fix dark-theme styles

--- src/main/java/ku/cs/controllers/general/AcceptAppealController.java
+++ src/main/java/ku/cs/controllers/general/AcceptAppealController.java
@@ -119,11 +119,12 @@ public class AcceptAppealController {
         approverTableView.getColumns().add(fullNameColumn);
 
         int size = approverTableView.getColumns().size();
+        double totalWidth = 448 - approverTableView.getPadding().getLeft() - approverTableView.getPadding().getRight();
+        approverTableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
         for (TableColumn<?, ?> col : approverTableView.getColumns()) {
-            col.setPrefWidth((double) 448 / size);
+            col.setPrefWidth(totalWidth / size);
         }
-        roleColumn.setPrefWidth(roleColumn.getPrefWidth() - 50);
-        fullNameColumn.setPrefWidth(fullNameColumn.getPrefWidth() - 50);
 
         approverTableView.getItems().clear();
         if (approverList != null) {
